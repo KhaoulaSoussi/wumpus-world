8a69f1cd965b100ef530fc8a368a0e1bddc86e53
LeilaMoussa <58636465+LeilaMoussa@users.noreply.github.com>
2022-03-07 20:30:20 +0100

    add some more rules (will likely need to be tweaked)

--- initial-states.pl
+++ initial-states.pl
@@ -1,6 +1,6 @@
 % Initial states common to all worlds.
 position(room(1, 1), 0).
-score(0, 0).
+score(0).
 has_arrows().
 wumpus_alive().
 PlayerAlive().
@@ -8,4 +8,6 @@ has_gold() :- false.
 visited(room(1, 1), 0).
 pit(room(1, 1)) :- false.
 wumpus(room(1, 1)) :- false.
+gold(room(1, 1)) :- false.
 did_shoot() :- false.
+did_grab() :- false.
--- main.pl
+++ main.pl
@@ -1,4 +1,5 @@
 % order of the rules needs to be verified
+% ideally, we would arrange them in terms of meaning (actions, sensors, score-keeping, etc.) while preserving correctness for search
 
 % i need to take a better look at these
 :- abolish(position/2). % room, time
@@ -8,7 +9,7 @@
 :- abolish(did_shoot/0).
 % player life too? score?
 
-% http://www.cse.unsw.edu.au/~billw/dictionaries/prolog/dynamic.html#:~:text=dynamic&text=In%20Prolog%2C%20a%20procedure%20is,loaded%20during%20the%20Prolog%20session.
+% See: http://www.cse.unsw.edu.au/~billw/dictionaries/prolog/dynamic.html#:~:text=dynamic&text=In%20Prolog%2C%20a%20procedure%20is,loaded%20during%20the%20Prolog%20session.
 :- dynamic([
   position/2,
   wumpus_alive/0,
@@ -26,7 +27,9 @@ world_A(4, 4).
 % world_B(4,4).
 
 % rooms are 1-indexed
-in_bounds(X, Y) :- X >= 1, X <= 4, Y >= 1, Y <= 4.
+%ERROR: c:/users/mouss/documents/wumpus-world/main.pl:28:29: Syntax error: Operator expected
+% i tried upper case variable names too.
+in_bounds(x, y) :- x >= 1, x <= 4, y >= 1, y <= 4.
 room(X, Y) :- in_bounds(X, Y).
 adjacent(room(X, Y), room(A, B)) :- A is X-1, B is Y ;
 									A is X+1, B is Y ; 
@@ -34,19 +37,14 @@ adjacent(room(X, Y), room(A, B)) :- A is X-1, B is Y ;
 									A is X, B is Y+1.
 safe(room(X, Y)) :- not(pit(room(X, Y)); wumpus(room(X, Y))).
 
-% should the initial score be defined in world builder? there are a lot of initial facts. maybe it is better to put them here because they are shared among worlds. or maybe we can put them in a file of their own.
-score(0, 0). % score is 0 at t=0
-% disregard the following line right now
-% score(S, H) :- score(Head + sum_list(Tail), [Head|Tail]).  % sum of list
-
 % SENSORS
 % might have to add ! to these for more efficient search
 breeze(room(X, Y)) :- adjacent(room(X, Y), room(A, B)), pit(room(A, B)).
 glitter(room(X, Y)) :- gold(room(X, Y)).
 stench(room(X, Y)) :- adjacent(room(X, Y), room(A, B)), wumpus(room(A, B)).
 % There is a bump if current position is a wall
-% is that useful? maybe if we think of orientation, which we are not right now.
-% Bump(room(X, Y)) :- .
+% is that useful? maybe it is if we think of orientation, which we are not right now.
+%Bump(room(X, Y)) :- .
 % There is a scream the moment wumpus is killed
 scream(T) :- kill(T), asserta(not(wumpus_alive())).
 
@@ -54,28 +52,51 @@ scream(T) :- kill(T), asserta(not(wumpus_alive())).
 move(room(X, Y), room(A, B), T) :- adjacent(room(X, Y), room(A, B)),
 								retractall(position(_, _)),
 								asserta(position(room(X, Y), T+1)),
-								assertz(visited(room(X, Y), T+1)).
-grab_gold(room(X, Y)) :- position(room(X, Y), T), gold(room(X, Y)),
+								assertz(visited(room(X, Y), T+1)),
+								score(S),
+								retractall(score(_)),
+								asserta(score(S - 1)).
+% We need a better understand of when to use timestamps.
+grab_gold(room(X, Y), T) :- position(room(X, Y), T), gold(room(X, Y)),
 						retractall(gold(_)),
-						retractall(glitter(_)),
-						retractall(did_grab()),
-						asserta(did_grab()),
-						score(S, T),
-						asserta(score(S+1000, T+1)), % add to score
-						asserta(position(room(X, Y), T+1)).  % this does not look good: it keeps the player in the same position in 2 consecutive moments because they grabbed gold
-shoot(room(X, Y), _) :- position(room(A, B)), adjacent(room(X, Y), room(A, B)), has_arrows(),
+						retractall(glitter(_)), % become false
+						score(S),
+						retractall(score(_)),
+						asserta(score(S + 1000 - 1)),
+						retractall(did_grab(_)),
+						asserta(did_grab()).
+shoot(room(X, Y), T) :- position(room(A, B), T), adjacent(room(X, Y), room(A, B)), has_arrows(),
 					retractall(did_shoot()),
-					asserta(did_shoot()).
+					asserta(did_shoot()),
+					score(S),
+					retractall(score(_)),
+					asserta(score(S - 10)).
 kill(T) :- shoot(room(X, Y), T), wumpus(room(X, Y)),
 		retractall(wumpus_alive()),
 		retractall(wumpus(room(X, Y))),
 		asserta(not(wumpus(room(X, Y)))). % to make the room safe
 
 has_arrows(_) :- not(did_shoot()).
-% player has gold (and can use it to get out of pits) if they grabbed some gold before and did not use it yet
+has_gold() :- did_grab(), not(climb()).
 
-has_gold(_) :- did_grab(_), not(used_gold(_)).
-climb_pit(room(X, Y)) :- position(room(X, Y), T), pit(room(X, Y)),
-				did_grab(_), has_gold(_), 
-				retractall(used_gold()),
-				asserta(used_gold()).
+%has_gold(_) :- did_grab(_), not(used_gold(_)).
+% climb_pit(room(X, Y)) :- position(room(X, Y), T), pit(room(X, Y)),
+% 				did_grab(_), has_gold(_), 
+% 				retractall(used_gold()),
+% 				asserta(used_gold()).
+
+fall(T) :- position(room(X, Y), T), pit(room(X, Y)),
+		score(S),
+		retractall(score(_)),
+		asserta(score(S - 1000)).
+climb(T) :- fall(T-1), has_gold(T-1),
+			retractall(has_gold(_)),
+			asserta(not(has_gold(_))),
+			score(S),
+			retractall(score(_)),
+			asserta(score(S - 1)).
+
+eaten(T) :- position(room(X, Y), T), wumpus(room(X, Y)),
+		score(S),
+		retractall(score(_)),
+		asserta(score(S - 1000)).
