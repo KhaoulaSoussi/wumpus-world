eb8203c313dc8b1e4ab74f2412ca06b15c39b10f
Khaoula Ait Soussi <85248282+KhaoulaSoussi@users.noreply.github.com>
2022-03-07 17:48:12 +0100

    Update main.pl

--- main.pl
+++ main.pl
@@ -17,22 +17,27 @@
   did_shoot/0
 ]).
 
+did_grab() :- false.
+used_gold() :- false.
+
 % we have to define different worlds in other pl files.
 world_A(4, 4).
 % can we pass filename for world as argument?
-%world_B(4,4).
+% world_B(4,4).
 
 % rooms are 1-indexed
-% Annoying error I don't know how to fix
-%ERROR: c:/users/mouss/documents/wumpus-world/main.pl:28:29: Syntax error: Operator expected
 in_bounds(X, Y) :- X >= 1, X <= 4, Y >= 1, Y <= 4.
 room(X, Y) :- in_bounds(X, Y).
-adjacent(room(X, Y), room(A, B)) :- A is X-1, B is Y ; A is X+1, B is Y ; A is X, B is Y-1 ; A is X, B is Y+1.
+adjacent(room(X, Y), room(A, B)) :- A is X-1, B is Y ;
+									A is X+1, B is Y ; 
+									A is X, B is Y-1 ; 
+									A is X, B is Y+1.
 safe(room(X, Y)) :- not(pit(room(X, Y)); wumpus(room(X, Y))).
+
 % should the initial score be defined in world builder? there are a lot of initial facts. maybe it is better to put them here because they are shared among worlds. or maybe we can put them in a file of their own.
 score(0, 0). % score is 0 at t=0
 % disregard the following line right now
-%score(S, H) :- score(Head + sum_list(Tail), [Head|Tail]).  % sum of list
+% score(S, H) :- score(Head + sum_list(Tail), [Head|Tail]).  % sum of list
 
 % SENSORS
 % might have to add ! to these for more efficient search
@@ -41,7 +46,7 @@ glitter(room(X, Y)) :- gold(room(X, Y)).
 stench(room(X, Y)) :- adjacent(room(X, Y), room(A, B)), wumpus(room(A, B)).
 % There is a bump if current position is a wall
 % is that useful? maybe if we think of orientation, which we are not right now.
-%Bump(room(X, Y)) :- .
+% Bump(room(X, Y)) :- .
 % There is a scream the moment wumpus is killed
 scream(T) :- kill(T), asserta(not(wumpus_alive())).
 
@@ -53,6 +58,8 @@ move(room(X, Y), room(A, B), T) :- adjacent(room(X, Y), room(A, B)),
 grab_gold(room(X, Y)) :- position(room(X, Y), T), gold(room(X, Y)),
 						retractall(gold(_)),
 						retractall(glitter(_)),
+						retractall(did_grab()),
+						asserta(did_grab()),
 						score(S, T),
 						asserta(score(S+1000, T+1)), % add to score
 						asserta(position(room(X, Y), T+1)).  % this does not look good: it keeps the player in the same position in 2 consecutive moments because they grabbed gold
@@ -66,4 +73,8 @@ kill(T) :- shoot(room(X, Y), T), wumpus(room(X, Y)),
 
 has_arrows() :- not(did_shoot()).
 % player has gold (and can use it to get out of pits) if they grabbed some gold before and did not use it yet
-%has_gold() :- .
+
+has_gold() :- did_grab(), not(used_gold()).
+climb_pit() :- did_grab(), has_gold(), 
+				retractall(used_gold()),
+				asserta(used_gold()).
