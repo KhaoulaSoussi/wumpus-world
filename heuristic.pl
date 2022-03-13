1135c76f0cb4d070cca968e0713d67b46bdd0e10
LeilaMoussa <58636465+LeilaMoussa@users.noreply.github.com>
2022-03-13 14:12:40 +0100

    finish safest

--- heuristic.pl
+++ heuristic.pl
@@ -52,7 +52,6 @@ heuristic(perceptions([yes, _, _, _])) :- position(room(X, Y), T), adjacent(room
 % khaoula
 % define explorable cells (visited + neighbors of visited)
 
-
 % leila
 % Breeze: just move to the best place
 heuristic([_, yes, _, _], move(room(X, Y), room(A, B), T)) :- tell_kb(breeze), current_safest_cell(room(A, B)).
@@ -63,7 +62,17 @@ heuristic([_, _, yes, _], grab_gold) :- tell_kb(glitter), !.
 
 % Leila
 % safest move (score)
-current_safest_cell(room(X, Y)) :- !. % find the room corresponding to the minimum score
+current_safest_cell(room(X, Y)) :- findall(S, total_current_score(room(A, B), S), L),
+                                  min_list(L, MinScore),
+                                  index_of(MinScore, L, Idx),
+                                  nth0(Idx, L, room(X, Y)).
+
+% util to get index of element
+% From reference:
+% index_of([H|_], H, 0):- !.
+% index_of([_|T], H, Index):- index_of(T, H, OldIndex), !, Index is OldIndex + 1.
+% I hope the following works:
+index_of(Elt, Lst, Idx) :- nth0(Idx, Lst, Elt).
 
 total_current_score(room(X, Y), T) :- findall(P, partial_score(room(X, Y), P), L), sum_list(L, T).
 
@@ -79,6 +88,6 @@ partial_score(room(X, Y), S) :- has_wumpus(room(X, Y), maybe), S is 800.
 % A bunch of atomic moves from X, Y to A, B
 % traveling from xy to ab through zw is equivalent to moving from xy to zw atomically then traveling from zw to ab
 % base case: same room
-% zw is xy's parent (hopefully)
+% zw is xy's parent
 travel(room(X, Y), room(A, B), T) :- room(Z, W), parent(room(Z, W), room(X, Y)), 
                     move(room(X, Y), room(Z, W), T), U is T+1, travel(room(Z, W), room(A, B), U), !.
